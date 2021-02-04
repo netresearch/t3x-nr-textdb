d544859d3f25c16912c146ab692fb0d8deecf7f5
Axel Seemann <axel.seemann@netresearch.de>
2021-02-04 15:31:08 +0100

    ATU-42: Remove unused templates. Remove f:be.container from layout otherwise the html tag is rendered twice.

--- Configuration/TypoScript/setup.ts
+++ Configuration/TypoScript/setup.ts
@@ -2,7 +2,7 @@
 # Module configuration
 module.tx_nrtextdb_web_nrtextdbtextdb {
     view {
-        templateRootPaths.0 = EXT:{extension.extensionKey}/Resources/Private/Backend/Templates/
+        templateRootPaths.0 = EXT:nr_textdb/Resources/Private/Backend/Templates/
         templateRootPaths.1 = {$module.tx_nrtextdb_textdb.view.templateRootPath}
         partialRootPaths.0 = EXT:nr_textdb/Resources/Private/Backend/Partials/
         partialRootPaths.1 = {$module.tx_nrtextdb_textdb.view.partialRootPath}
