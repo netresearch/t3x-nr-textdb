5fd5cd8746038fae951ce74eca43d1f97634e0f7
Rico Sonntag <rico.sonntag@netresearch.de>
2025-05-09 10:37:03 +0000

    NASAFF-259: Refactor code to be compatible with TYPO3 v13

--- Classes/Controller/TranslationController.php
+++ Classes/Controller/TranslationController.php
@@ -30,8 +30,8 @@ use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotCon
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Http\UploadedFile;
-use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Imaging\IconSize;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use TYPO3\CMS\Core\Messaging\FlashMessage;
 use TYPO3\CMS\Core\Messaging\FlashMessageService;
@@ -889,7 +889,7 @@ class TranslationController extends ActionController
 
             $icon = $this->iconFactory->getIcon(
                 $tableConfiguration['icon'],
-                Icon::SIZE_SMALL
+                IconSize::SMALL
             );
 
             $viewButton = $buttonBar
